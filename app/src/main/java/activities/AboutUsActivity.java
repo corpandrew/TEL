0e10ee11b0400e02e058f36cbd8761fb74c26c85
Andrew Corp <corpa@wit.edu>
2016-10-06 16:46:43 -0400

    forgot to update the version code changed the applicationId to "techxlab"

--- app/src/main/java/activities/AboutUsActivity.java
+++ app/src/main/java/activities/AboutUsActivity.java
@@ -1,16 +1,11 @@
 package activities;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import corp.andrew.tel.R;
 
